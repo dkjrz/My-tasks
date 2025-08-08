d7bbc781f0adc5bebccf8aef2f5c11b526bebf3b
Dmitry D. <dagdarim666@gmail.com>
2025-08-08 22:12:53 +0300

    Add files via upload

---
+++ multilingual-site/lang.js
@@ -0,0 +1,44 @@
+const langArr = {
+    'title': {
+        'uk': 'Мультимовна сторінка',
+        'en': 'Multilingual Page',
+        'de': 'Mehrsprachige Seite',
+        'fr': 'Page multilingue',
+    },
+    'text': {
+        'uk': 'Це приклад мультимовної сторінки.',
+        'en': 'This is an example of a multilingual page.',
+        'de': 'Dies ist ein Beispiel für eine mehrsprachige Seite.',
+        'fr': 'Ceci est un exemple de page multilingue.',
+    },
+    'name': {
+        'uk': 'Ноутбук Lenovo IdeaPad',
+        'en': 'Lenovo IdeaPad Laptop',
+        'de': 'Lenovo IdeaPad Laptop',
+        'fr': 'Ordinateur portable Lenovo IdeaPad',
+    },
+    'os': {
+        'uk': 'Операційна система',
+        'en': 'Operating System',
+        'de': 'Betriebssystem',
+        'fr': 'Système d’exploitation',
+    },
+    'memory': {
+        'uk': "Об’єм пам’яті",
+        'en': 'Memory Size',
+        'de': 'Speicherkapazität',
+        'fr': 'Taille de la mémoire',
+    },
+    'type': {
+        'uk': 'Тип пам’яті',
+        'en': 'Memory Type',
+        'de': 'Speicherart',
+        'fr': 'Type de mémoire',
+    },
+    'color': {
+        'uk': 'Колір',
+        'en': 'Color',
+        'de': 'Farbe',
+        'fr': 'Couleur',
+    },
+};
---
+++ multilingual-site/script.js
@@ -0,0 +1,29 @@
+const select = document.querySelector('#select');
+const allLang = ['uk', 'en', 'fr', 'de'];
+
+const changeUrLang = () => {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    changeLanguage();
+
+};
+
+select.addEventListener('change', changeUrLang);
+
+const changeLanguage = () => {
+    let hash = window.location.hash
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#' + 'uk';
+        hash = 'uk';   
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langArr['title'][hash];
+    for(let key in langArr) {
+        document.querySelector(`.lang-${key}`).innerHTML = langArr[key][hash];
+    }
+    
+
+};
+
+changeLanguage();
